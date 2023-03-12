8f591ce1707b99484a436fdc3059978b1973a686
vinay santosh <3748745+vinaysantosh@users.noreply.github.com>
2023-03-12 12:21:01 -0500

    commit to add round

--- bmicalculator.py
+++ bmicalculator.py
@@ -3,6 +3,6 @@
 height = float(input("Enter your height in m : "))
 weight = float(input ("Enter your weight in kg : "))
 
-bmi = int(weight/(height**2))
+bmi = round(weight/(height**2))
 
 print("Your BMI is: " + str(bmi))
