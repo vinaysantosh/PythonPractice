b319839087c5978e197ff50fb142ccffbab1f6eb
vinay santosh <3748745+vinaysantosh@users.noreply.github.com>
2023-03-12 12:37:00 -0500

    can ride roller coater program

---
+++ canrideroller.py
@@ -0,0 +1,8 @@
+# Asks for an input of the height of the person and determines if they can ride the rollercoaster
+
+height = int(input("Enter your height in cm : "))
+
+if (height>120):
+    print("You can ride the roller coaster !")
+else: 
+    print("You are not tall enough to ride the roller coaster !")
