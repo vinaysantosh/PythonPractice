935d577ec677dc6a24e70c918f0c1072d41971f3
vinay santosh <3748745+vinaysantosh@users.noreply.github.com>
2023-03-12 12:26:33 -0500

    life left program

---
+++ lifeleft.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age : "))
+
+years_left = 90-age
+months_left = 12*years_left
+weeks_left = 52*years_left
+days_left = 365*years_left
+
+print(f"you have {months_left} months left or {weeks_left} weeks left or {days_left} days left")
