1b1cad096aff305c123dab082e22d54cb33c3ad8
vinay santosh <3748745+vinaysantosh@users.noreply.github.com>
2023-03-12 11:52:10 -0500

    band name generator

--- bandname.py
+++ bandname.py
@@ -1,6 +1,6 @@
 # This program asks for your city and pet name and returns the name of a band
 
-city=input("Which city were you born in?")
-pet_name=input("What is the name of your pet?")
+city=input("Which city were you born in?\n")
+pet_name=input("What is the name of your pet?\n")
 
 print("Your band name can be " + city + " " + pet_name)
