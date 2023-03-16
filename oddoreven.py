9a9bd9af2f56ce9bdaab3b5f0e22b05fb0ae239a
vinay santosh <3748745+vinaysantosh@users.noreply.github.com>
2023-03-16 10:21:37 -0700

    oddor even

---
+++ oddoreven.py
@@ -0,0 +1,7 @@
+# This program checks if a number input by the user is even or odd
+user_input=int(input("Please neter a number to check if it is odd or even : "))
+
+if (user_input%2 == 1) :
+    print("The number is odd")
+else :
+    print("The number is even")
